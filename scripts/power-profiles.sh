17b0c1ef3e076e0795716a330b23505fe522a1d4
hengtseChou <hankthedev@gmail.com>
2024-11-26 23:49:27 +0800

    feat: keybind and power profile menu for fuzzel

--- scripts/power-profiles.sh
+++ scripts/power-profiles.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-action=$1
-if [ "$action" == "get" ]; then
-  notify-send "power-profiles-daemon" "current active: $(powerprofilesctl get)"
-elif [ "$action" == "set" ]; then
-  options="power-saver\nbalanced\nperformance"
-  choice=$(echo -e "$options" | rofi -dmenu -replace -config $HOME/Niri/rofi/config-power.rasi -i -no-show-icons -l 3 -width 30 -p "Select Power Profile")
-  if [ ! -z "$choice" ]; then
-    powerprofilesctl set $choice
-  fi
+options="power-saver\nbalanced\nperformance"
+choice=$(echo -e "$options" | fuzzel --dmenu --lines 3)
+if [ ! -z "$choice" ]; then
+  powerprofilesctl set $choice
 fi
