86cf7c784ddc90a553c4763de5f67ea28ad8030b
hengtseChou <hankthedev@gmail.com>
2024-11-28 13:54:58 +0800

    feat: specify fuzzel config path in commands

--- scripts/power-profiles.sh
+++ scripts/power-profiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 options="power-saver\nbalanced\nperformance"
-choice=$(echo -e "$options" | fuzzel --dmenu --lines 3)
+choice=$(echo -e "$options" | fuzzel --dmenu --lines 3  --config $HOME/Niri/fuzzel/fuzzel.ini)
 if [ ! -z "$choice" ]; then
   powerprofilesctl set $choice
 fi
