1ae768000312f04516f774a73539876fc3aef977
hengtseChou <hankthedev@gmail.com>
2024-12-16 18:04:15 +0800

    fix: bring updater tooltip back

--- waybar/modules/count-updates.sh
+++ waybar/modules/count-updates.sh
@@ -2,4 +2,4 @@
 updates_pacman=$(checkupdates | wc -l)
 updates_aur=$(paru -Qua | grep -v "\[ignored\]" | wc -l)
 updates=$((updates_pacman + updates_aur))
-printf '{"text": "%s", "alt": "%s", "tooltip": "%s Updates"}' "$updates" "$updates" "$updates"
+printf '{"text": "%s", "alt": "%s", "tooltip": "%s updates"}' "$updates" "$updates" "$updates"
