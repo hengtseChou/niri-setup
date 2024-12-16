7ce9d3e9760f98e346638ab32f2d8e1de6eecd96
hengtseChou <hankthedev@gmail.com>
2024-12-16 17:04:03 +0800

    style: rename change power profile script

--- setup.sh
+++ setup.sh
@@ -117,9 +117,9 @@ config_folder=$(dirname "$(realpath "$0")")
 symlink $config_folder/niri --to-config
 
 sed -i "s|\$NIRICONF|$config_folder|g" $(realpath "$config_folder/niri/config.kdl")
-sed -i "s|\$NIRICONF|$config_folder|g" $(realpath "$config_folder/scripts/power-profiles.sh")
-sed -i "s|\$NIRICONF|$config_folder|g" $(realpath "$config_folder/scripts/swayidle.sh")
 sed -i "s|\$NIRICONF|$config_folder|g" $(realpath "$config_folder/scripts/change-idle-time.sh")
+sed -i "s|\$NIRICONF|$config_folder|g" $(realpath "$config_folder/scripts/change-power-profile.sh")
+sed -i "s|\$NIRICONF|$config_folder|g" $(realpath "$config_folder/scripts/swayidle.sh")
 sed -i "s|\$NIRICONF|$config_folder|g" $(realpath "$config_folder/scripts/toggle-waybar.sh")
 sed -i "s|\$NIRICONF|$config_folder|g" $(realpath "$config_folder/scripts/wlogout.sh")
 sed -i "s|\$NIRICONF|$config_folder|g" $(realpath "$config_folder/waybar/config")
