eb45086a7842096f037662cb19e952a6930cffbc
hengtseChou <hankthedev@gmail.com>
2024-12-25 16:43:12 +0800

    style: updater text message

--- waybar/modules/install-updates.sh
+++ waybar/modules/install-updates.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo ""
 figlet "Updates" -f slant -w 44 -c
 echo ""
 sleep 0.1
@@ -7,6 +6,6 @@ sleep 0.1
 paru
 
 notify-send "Update complete"
-echo
-echo ":: Update complete"
+echo ""
+echo "[INFO] OK"
 sleep 2
