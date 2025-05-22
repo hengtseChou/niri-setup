0d5b101ea6102b25ec890a724d82e2b6fdb8cd91
hengtseChou <hankthedev@gmail.com>
2025-05-22 23:10:51 +0800

    fix: align msg to lower cases

--- scripts/change-wallpaper.sh
+++ scripts/change-wallpaper.sh
@@ -9,19 +9,19 @@ if [ ! -d "$wallpaper_dir" ]; then
 fi
 images=$(fd . --base-directory "$wallpaper_dir" -x file {} | grep -oP '^.+: \w+ image' | cut -d ':' -f 1 | sort)
 if [ -z "$images" ]; then
-  echo "[ERROR] No image file found. Place your wallpapers in $wallpaper_dir."
+  echo "[ERROR] no image file found. place your wallpapers in $wallpaper_dir."
   exit 1
 fi
-image="$wallpaper_dir/$(echo "$images" | gum choose --header 'Choose your wallpaper: ')"
+image="$wallpaper_dir/$(echo "$images" | gum choose --header 'choose your wallpaper: ')"
 if [ $? -eq 1 ]; then
   exit 1
 fi
-mode=$(echo -e "stretch\nfill\nfit\ncenter\ntile" | gum choose --header "Choose wallpaper mode: ")
+mode=$(echo -e "stretch\nfill\nfit\ncenter\ntile" | gum choose --header "choose wallpaper mode: ")
 if [ $? -eq 1 ]; then
   exit 1
 fi
 
-echo "[INFO] Copying selected wallpaper to $NIRICONF..."
+echo "[INFO] copying selected wallpaper to $NIRICONF..."
 cp $image "$NIRICONF/wallpapers/workspace.${image##*.}"
 canvas_color=$(magick $NIRICONF/wallpapers/workspace.${image##*.} -crop x1+0+0 -resize 1x1 txt:- | grep -o '#[0-9A-Fa-f]\{6\}')
 workspace_cmd="\"swaybg\" \"-i\" \"$NIRICONF/wallpapers/workspace.${image##*.}\" \"-m\" \"$mode\" \"-c\" \"$canvas_color\""
@@ -29,10 +29,10 @@ sed -i "s|^spawn-at-startup \"swaybg.*|spawn-at-startup $workspace_cmd|" "$NIRIC
 pkill swaybg
 (eval "$workspace_cmd" &>/dev/null &)
 
-echo "[INFO] Creating new overview backdrop..."
+echo "[INFO] creating new overview backdrop..."
 magick "$NIRICONF/wallpapers/workspace.${image##*.}" -blur 0x8 "$NIRICONF/wallpapers/backdrop.${image##*.}"
 backdrop_cmd="\"swww\" \"img\" \"$NIRICONF/wallpapers/backdrop.${image##*.}\""
 swww img "$NIRICONF/wallpapers/backdrop.${image##*.}"
 sed -i "s|^spawn-at-startup \"swww.*|spawn-at-startup $backdrop_cmd|" "$NIRICONF/niri/config.kdl"
 
-echo "[INFO] Done!"
+echo "[INFO] done!"
--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-SKIP_INSTALL=false
 export GUM_CHOOSE_HEADER_FOREGROUND="$#d8dadd"
 export GUM_CHOOSE_SELECTED_FOREGROUND="#758A9B"
 export GUM_CHOOSE_CURSOR_FOREGROUND="#758A9B"
@@ -8,92 +7,12 @@ is_installed() {
   pacman -Qi "$1" &>/dev/null
 }
 
-symlink() {
-  source="$1"
-  shift
-  PARSED=$(getopt -o '' --long to-home,to-config,custom-dir: -- "$@")
-  if [[ $? -ne 0 ]]; then
-    return 1
-  fi
-  eval set -- "$PARSED"
-
-  target_dir=""
-  while true; do
-    case "$1" in
-    --to-home)
-      target_dir="$HOME"
-      shift
-      ;;
-    --to-config)
-      target_dir="$HOME/.config"
-      shift
-      ;;
-    --custom-dir)
-      target_dir="$2"
-      mkdir -p $target_dir
-      shift 2
-      ;;
-    --)
-      shift # End of options
-      break
-      ;;
-    *)
-      echo "[Error] Invalid option: $1" >&2
-      return 1
-      ;;
-    esac
-  done
-
-  # If no valid options were provided (target_dir is empty), show an error and exit
-  if [ -z "$target_dir" ]; then
-    echo "[Error] target dir $target_dir is empty"
-    return 1
-  fi
-
-  target="$target_dir/$(basename $source)"
-
-  if [ -L "${target}" ]; then
-    # is a symlink
-    rm ${target}
-    ln -s ${source} ${target}
-  elif [ -d ${target} ]; then
-    # is a dir
-    rm -rf ${target}/
-    ln -s ${source} ${target}
-  elif [ -f ${target} ]; then
-    # is a file
-    rm ${target}
-    ln -s ${source} ${target}
-  else
-    ln -s ${source} ${target}
-  fi
-}
-
-PARSED=$(getopt -o '' --long skip-install -- "$@")
-if [[ $? -ne 0 ]]; then
-  exit 1
+SKIP_INSTALL=false
+if [ "$1" == "--skip-install" ]; then
+  SKIP_INSTALL=true
 fi
-eval set -- "$PARSED"
-
-while true; do
-  case "$1" in
-  --skip-install)
-    SKIP_INSTALL=true
-    shift
-    ;;
-  --)
-    shift
-    break
-    ;;
-  *)
-    echo "[Error] Invalid option: $1" >&2
-    exit 1
-    ;;
-  esac
-done
 
 if [ "$SKIP_INSTALL" = false ]; then
-
   if ! is_installed gum; then
     echo "[ERROR] missing dependency: gum"
     exit 1
@@ -115,7 +34,7 @@ if [ "$SKIP_INSTALL" = false ]; then
     echo "[ERROR] no AUR helper available. please install one of {yay, paru, aura, trizen}."
     exit 1
   fi
-  aur=$(gum choose "${available_helpers[@]}" --header "Choose an AUR helper:" --select-if-one)
+  aur=$(gum choose "${available_helpers[@]}" --header "choose an AUR helper:" --select-if-one)
 
   pkgs=(
     alacritty
@@ -141,7 +60,16 @@ if [ "$SKIP_INSTALL" = false ]; then
 fi
 
 config_folder=$(dirname "$(realpath "$0")")
-symlink $config_folder/niri --to-config
+if [ -d "$HOME/.config/niri" ]; then
+  printf "[WARN] niri config found. do you want to overwrite it? (Y/n): "
+  read -r overwrite
+  overwrite=${overwrite:-Y}
+  if [[ $overwrite =~ ^[Yy]$ ]]; then
+    ln -sf $config_folder/niri $HOME/.config
+  else
+    exit 0
+  fi
+fi
 
 sed -i "s|\$NIRICONF|$config_folder|g" $(realpath "$config_folder/niri/config.kdl")
 sed -i "s|\$NIRICONF|$config_folder|g" $(realpath "$config_folder/scripts/change-idle-time.sh")
@@ -153,9 +81,9 @@ sed -i "s|\$NIRICONF|$config_folder|g" $(realpath "$config_folder/scripts/wlogou
 sed -i "s|\$NIRICONF|$config_folder|g" $(realpath "$config_folder/waybar/config")
 sed -i "s|\$NIRICONF|$config_folder|g" $(realpath "$config_folder/waybar/modules.jsonc")
 
-if niri validate >/dev/null; then
+if niri validate &>/dev/null; then
   echo "[INFO] niri setup all completed"
 else
-  echo "[ERROR] Something went wrong. See the following output:"
+  echo "[ERROR] something went wrong. see the following output:"
   niri validate
 fi
